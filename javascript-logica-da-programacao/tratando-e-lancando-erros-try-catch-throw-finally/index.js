b78f9738f5350fecf16939fa76694a0bebdd3bec
Tiago Sebastian <tiagosebastian99@gmail.com>
2023-10-17 16:12:44 -0400

    error,logica de programacão

---
+++ javascript-logica-da-programacao/tratando-e-lancando-erros-try-catch-throw-finally/index.js
@@ -0,0 +1,29 @@
+function retonaHora (data){
+    if (data && ! (data instanceof Date)){
+        throw new TypeError('Esperando instancia de Date');
+    }
+
+    if(!data){
+        data = new Date();
+    
+}
+
+return data.toLocaleTimeString('pt-BR', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+});
+
+}
+
+try {
+    const data = new Date ('01-01-1970  18:28:55');
+    const hora = retonaHora();
+    console.log(hora);
+} catch(e){
+    //tratar erro
+    //console.log(e);
+} finally{
+    console.log('Tenha um bom dia.');
+}
