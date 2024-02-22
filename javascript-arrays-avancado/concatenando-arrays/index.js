5afa544889f7d67e73aeecbcbf6084085fcf6e6a
Tiago Sebastian <tiagosebastian99@gmail.com>
2024-02-22 14:10:57 -0400

    metodo splice

---
+++ javascript-arrays-avancado/concatenando-arrays/index.js
@@ -0,0 +1,8 @@
+//
+const nomes =  ['Maria', 'Joao','Eduardo', 'Gabriel', 'Julia']
+
+
+// nomes.splice(indice, delete, elem1, elem2, elem3);
+// pop
+const removidos = nomes.splice(4, 1);
+console.log(nomes, removidos);
---
+++ javascript-arrays-avancado/metodo-splice/index.js
@@ -0,0 +1,17 @@
+//
+const nomes =  ['Maria', 'Joao','Eduardo', 'Gabriel', 'Julia']
+
+//push
+//nomes.splice(nomes.length, 0, 'Tiago', 'Sebastian');
+//console.log(nomes);
+
+//Unshift
+nomes.splice(0, 0, 'Tiago', 'Sebastian');
+console.log(nomes);
+
+// nomes.splice(indice, delete, elem1, elem2, elem3);
+// pop
+//const removidos = nomes.splice(4, 1);
+
+//shift
+// const removidos = nomes.splice(0, 1);
